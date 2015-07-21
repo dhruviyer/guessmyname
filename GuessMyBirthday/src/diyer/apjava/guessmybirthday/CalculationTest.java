fcca63f5d8047bea77cffc1a2ba62032ac0b1be0
Dhruv <northstardude@gmail.com>
2015-07-21 07:38:20 -0700

    Completed the aesthetic portion of the main class

--- GuessMyBirthday/src/diyer/apjava/guessmybirthday/CalculationTest.java
+++ GuessMyBirthday/src/diyer/apjava/guessmybirthday/CalculationTest.java
@@ -14,7 +14,9 @@ public class CalculationTest {
 		str=str+"1. Take the number of the month that you were born 1-jan 2-Feb, etc"+'\n';
 		str=str+"2. Double the month number" + '\n';
 		str=str+"3. Next add 6"+'\n';
-		
+		str=str+"4. Multiply the result by 50"+'\n';
+		str=str+"5. Add the dat of the month tha you were born" + '\n';
+		str=str+"6. Subtract 365 from the number"+'\n';
 		JOptionPane.showMessageDialog(null,str);
 	}
 }
