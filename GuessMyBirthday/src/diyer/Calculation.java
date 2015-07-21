b3e8af72deb5dd102f01e5edf5b79d5e78ebcf13
Dhruv <northstardude@gmail.com>
2015-07-21 07:29:16 -0700

    Created package diyer

---
+++ GuessMyBirthday/src/diyer/Calculation.java
@@ -0,0 +1,5 @@
+package diyer;
+
+public class Calculation {
+
+}
---
+++ GuessMyBirthday/src/diyer/CalculationTest.java
@@ -0,0 +1,16 @@
+package diyer;
+
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
+
+public class CalculationTest {
+	public static void main(String[] args) throws IOException{
+		String str="";
+		str=str+"This program will guess your birthday and month"+'\n';
+		str=str+"Answer the following questions and do the simple math" +'\n'+'\n';
+		
+		JOptionPane.showMessageDialog(null,str);
+	}
+}
