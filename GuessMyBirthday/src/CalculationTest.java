7fa2292b1edc1bca0269ba034f0136b11639e386
Dhruv <northstardude@gmail.com>
2015-07-20 21:07:02 -0700

    Added first string

--- GuessMyBirthday/src/CalculationTest.java
+++ GuessMyBirthday/src/CalculationTest.java
@@ -1,8 +1,12 @@
 import java.io.IOException;
 
+import javax.swing.JOptionPane;
+
 
 public class CalculationTest {
 	public static void main(String[] args) throws IOException{
-		
+		String str="";
+		str=str+"This program will guess your birthday and month"+'\n';
+		JOptionPane.showMessageDialog(null,str);
 	}
 }
